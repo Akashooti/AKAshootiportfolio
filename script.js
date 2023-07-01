e60997a80c6ac240c7e7c2133317edc7eece9d1c
Akashooti <107711784+Akashooti@users.noreply.github.com>
2023-07-01 14:19:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// var js = "javscript";
+// if(js === 'javscript') alert('this is javascript');
+// if(js === 'python') alert('this is python');
+
+40+50-56-10+40;
+console.log(40+40+23-50-56);
+
+let name = "akash";
+console.log(name);
+
+let PI = 3.1416;
+console.log(PI);
+
+let fristname = "flase"
+console.log(fristname);
+
+console.log(typeof fristname);
+console.log(typeof 23);
+console.log(typeof 'fadada');
+console.log(typeof c);
+console.log(typeof null);
+
+ let x = 20;
+ let y = 56;
+ console.log(x);
+x++;
+x--;
+x>y;
+x<y;
+x>=y;
+x<=y;
+x===y;
+x!=y;
+
+
+let markheight = 7;
+let jhonheight = 6;
+let jhonmass = 25;
+let markmass = 70
+let bmi = (markmass/markheight)+jhonheight/jhonmass;
+if(markheight>jhonheight) console.log('mark is highr than jihn');
+
+
+const jj = 'dsksk';
+const birthyear = 2001;
+const year = 2000;
+const sk = 'dcldksl';
+
+//const lk = 'kl ${jj}, ksj{birthyear-}
+
+
+
+
+
+
